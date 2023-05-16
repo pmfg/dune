4cb4ae9206b827b3c474c8b005dfb07b0d144646
Pedro Gonçalves <pedro@lsts.pt>
2023-05-16 10:02:46 +0100

    Cmake: add check of gcc greater of 7 to ignore warnings.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -137,6 +137,10 @@ macro(dune_probe_cxx)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pg")
       endif(PROFILE)
 
+      if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7)
+        set(DUNE_CXX_FLAGS "${DUNE_CXX_FLAGS} -Wno-psabi")
+      endif(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7)
+
       set(DUNE_CXX_FLAGS_STRICT "-Wall -Wshadow -pedantic")
       set(DUNE_C_FLAGS_STRICT "-Wall -Wshadow -pedantic")
       set(DUNE_CXX_FLAGS_LOOSE  "")
